2c7dad7b4286208f4da02243a70158d49fcdf18d
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-11-12 11:05:37 +0500

    code improvement

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,7 +1,58 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import Button from "../../components/shared/button/Button";
+import { useState } from "react";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+  });
+  const [error, setError] = useState({
+    name: false,
+    email: false,
+  });
+
+  const validation = () => {
+    if (!values.name) {
+      setError({
+        ...error,
+        name: true,
+      });
+    }
+    if (!values.email) {
+      setError({
+        ...error,
+        email: true,
+      });
+    }
+
+    if (!values.name || !values.email) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validation()) {
+      console.log("welcome");
+      navigate("/student-list");
+    } else {
+      console.log("error", error);
+      console.log("ingo", values);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
   return (
     <div className="w-full flex items-center h-screen">
       <div className=" sm:max-w-[500px] m-4 border border-chineseViolet p-4 xs:p-6 sm:p-10 md:p-16 mx-auto">
@@ -9,19 +60,29 @@ const Login = () => {
           <h1 className="mb-8 xl:mb-10 text-chineseViolet font-semibold text-xl sm:text-2xl xl:text-4xl">
             Login
           </h1>
-          <input
-            type="text"
-            className="border block mb-2 pl-1 mx-auto rounded-lg border-chineseViolet w-full h-10 text-lg"
-            placeholder="Name"
-          />
-          <input
-            type="email"
-            className="border block mb-8 pl-1 mx-auto rounded-lg border-chineseViolet w-full h-10 text-lg"
-            placeholder="Email"
-          />
-          <NavLink to="/student-list">
-            <Button className="w-full mb-2">Sign In</Button>
-          </NavLink>
+          <form className="w-full" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="border block mb-2 pl-1 mx-auto rounded-lg border-chineseViolet w-full h-10 text-lg"
+              placeholder="Name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              className="border block mb-8 pl-1 mx-auto rounded-lg border-chineseViolet w-full h-10 text-lg"
+              placeholder="Email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {/* <NavLink to="/student-list"> */}
+            <Button className="w-full mb-2" type="submit">
+              Sign In
+            </Button>
+            {/* </NavLink> */}
+          </form>
 
           <NavLink to="/sign-up">
             <Button className="w-full mb-2">Create New Account</Button>
