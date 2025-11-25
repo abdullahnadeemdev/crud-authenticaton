71f61ef80f0ba9a8e6db62ccf5f8162f7abd790f
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-11-25 19:02:41 +0500

    initialized redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvide } from "./context/authContext.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvide>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvide>
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <AuthProvide>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvide>
+    </StrictMode>
+  </Provider>
 );
